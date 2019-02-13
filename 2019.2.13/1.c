edd9bd473237ee9156202a5fc64c86a767843c6d
wang2470616413 <2470616413@qq.com>
2019-02-13 08:20:11 +0800

    2019.2.13campDay2

---
+++ 2019.2.13/1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<algorithm>
+#define mmset(a,b) memset(a,b,sizeof(a))
+using namespace std;
+const int N = 6005;
+/*
+ 3
+3
+6 6 6
+4
+21 12 12 21
+2
+998244353 985661441
+*//
+int main()
+{
+
+
+
+
+
+
+
+	return 0:
+}
+		
