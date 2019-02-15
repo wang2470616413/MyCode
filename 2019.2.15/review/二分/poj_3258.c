cfac1487c8b66ae2556cc0d538d5927395b5f468
wang2470616413 <2470616413@qq.com>
2019-02-15 20:57:20 +0800

    poj_258

--- 2019.2.15/review/二分/poj_3258.c
+++ 2019.2.15/review/二分/poj_3258.c
@@ -3,12 +3,59 @@
 #include<algorithm>
 #define ll long long 
 #define mmset(a,b) memset(a,b,sizeof(a))
-uing namespace std;
+using namespace std;
+const int N = 5e4 + 5;
+int data[N],L,n,k;
+int count1(int dis)
+{
+	int sum = 0;
+	int temp = 0;
+	for(int i = 1; i <= n; i++)
+	{
+		if(data[i] - data[i-1] + temp < dis)
+		{
+			sum++;
+			temp += data[i] - data[i-1];
+		}
+		else
+		{
+			temp = 0;
+		}
+	}
+	return sum; 
+}
 int main()
 {
+	while(~scanf("%d %d %d",&L,&n,&k))
+	{
+		for(int i = 1; i <= n; i++)
+		{
+			scanf("%d",&data[i]);
+		}
+		data[0] = 0, data[n+1] = L;
+		int a = 0, b = 2 * L;
+		int m;
+		while(a < b)
+		{
+			m = (a + b) / 2;
+			int key = count1(m);
+			if(key >= k)
+			{
+				a = m;
+			}
+			else 
+			{
+				b = m;
+			}
+
+		}
+		printf("1\n");
 
 
+		printf("%d\n",m);
 
+
+	}
 
 
 	return 0;
