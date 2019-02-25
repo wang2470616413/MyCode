c67475d36b57d5b94ce9ad0a80a23f6df14db2f9
wang2470616413 <2470616413@qq.com>
2019-02-26 00:16:38 +0800

    D.c

--- 2019.2.25/D.c
+++ 2019.2.25/D.c
@@ -1,50 +1,47 @@
 #include<stdio.h>
 #include<string.h>
+#include<vector>
 #include<algorithm>
 #define ll long long 
 #define mmset(a,b) memset(a,b,sizeof(a))
 using namespace std;
 const int N = 105, M = 200;
-struct node
-{
-	int k,num;
-	node()
-	{
-		k = N;
-		num = 0;
-	};
-	node(int pk,int pnum)
-	{
-		k = pk;
-		num = pnum;
-	}
-	bool operator < (const node b) const 
-	{
-		return num > b.num;
-	}
-};
-node data[N];
 int n,m;
-int count1(int mark);
+vector <int> data[N];
+int aux[N];
 int main()
 {
+	
 	scanf("%d %d",&n,&m);
 	for(int i = 1; i <= m; i++)
 	{
 		int a,b;
 		scanf("%d %d",&a,&b);
-		if(data[a].k > b)
-		{
-			data[a].k = b;
-		}
-		data[a].num++;
+		data[a].push_back(b);
 	}
 	for(int i = 1; i <= n; i++)
 	{
-		int res = count1(i);
-		printf("%d ",res);
+		int min1 = 99999999;
+		for(int j = 0; j < data[i].size(); j++)
+		{
+			int temp = i < data[i][j] ? data[i][j] - i : n - i + data[i][j];
+			min1 = min(min1,temp);
+		}
+		aux[i] = min1;
+	}	
+	for(int i = 1; i <= n; i++)
+	{
+		int max1 = -1;
+		for(int j = 1; j <= n; j++)if(data[j].size() > 0)
+		{
+			int dis = j >= i ? j - i : n - i + j;
+			int temp = dis + n * (data[j].size() - 1) + aux[j];
+			max1 = max(max1,temp);
+		}
+		printf("%d ",max1);
 	}
 	printf("\n");
+
 
 	return 0;
 }
