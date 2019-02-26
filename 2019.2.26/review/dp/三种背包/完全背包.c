39dabf554cb5910dd968b6f6448bf3c75c8d2d87
wang2470616413 <2470616413@qq.com>
2019-02-26 15:03:29 +0800

    完全背包.c

--- 2019.2.26/review/dp/三种背包/完全背包.c
+++ 2019.2.26/review/dp/三种背包/完全背包.c
@@ -4,7 +4,7 @@
 #define ll long long 
 #define mmset(a,b) memset(a,b,sizeof(a))
 using namespace std;
-const int N = 105; M = 1005;
+const int N = 105, M = 1005;
 int n,m;
 int V[N],W[N];
 int dp[M];
@@ -19,8 +19,19 @@ int main()
 	{
 		scanf("%d",&V[i]);
 	}
-
-
+	mmset(dp,0);
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = V[i]; j <= m; j++)
+		{
+			dp[j] = max(dp[j],dp[j - V[i]] + W[i]);
+		}
+	}
+	for(int i = 1; i <= m; i++)
+	{
+		printf("%d ",dp[i]);
+	}
+	printf("\n");
 
 
 	return 0;
