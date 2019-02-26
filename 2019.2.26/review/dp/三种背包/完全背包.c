6726abb999c261bf72cad9d9d09cc5c7ea617a79
wang2470616413 <2470616413@qq.com>
2019-02-26 14:40:35 +0800

    完全背包.c

---
+++ 2019.2.26/review/dp/三种背包/完全背包.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<algorithm>
+#define ll long long 
+#define mmset(a,b) memset(a,b,sizeof(a))
+using namespace std;
+const int N = 105; M = 1005;
+int n,m;
+int V[N],W[N];
+int dp[M];
+int main()
+{
+	scanf("%d %d",&n,&m);
+	for(int i = 1; i <= n; i++)
+	{
+		scanf("%d",&W[i]);
+	}
+	for(int i = 1; i <= n; i++)
+	{
+		scanf("%d",&V[i]);
+	}
+
+
+
+
+	return 0;
+}
