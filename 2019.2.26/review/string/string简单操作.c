5e6f49a15444c63bb85ea64753b486c6790b8449
wang2470616413 <2470616413@qq.com>
2019-02-26 10:58:50 +0800

    string简单操作.c

--- 2019.2.26/review/string/string简单操作.c
+++ 2019.2.26/review/string/string简单操作.c
@@ -4,9 +4,12 @@
 using namespace std;
 int main()
 {
-
-
-
-
-	return 0 ;
+//一。初始化
+	string str1("123456",2,2);
+	cout<<str1<<endl; //从小标2开始，长度为2的字符，输出34
+//,查找
+	string str2 = "11222334667894";
+	int index1 = str2.find("222"); //从头开始查找，返回"222"第一次出现的下标，如果没有的话，返回string::npos 
+	cout<<index1<<endl;
+	return 0;
 }
