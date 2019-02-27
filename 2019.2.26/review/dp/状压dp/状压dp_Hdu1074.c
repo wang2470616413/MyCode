7409521e693b424f9985b2e67b05a4c97c3f7242
wang2470616413 <2470616413@qq.com>
2019-02-27 23:46:48 +0800

    状压dp_Hdu1074.c

---
+++ 2019.2.26/review/dp/状压dp/状压dp_Hdu1074.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#include<string>
+#include<iostream>
+#include<algorithm>
+#define ll long long 
+#define mmset(a,b) memset(a,b,sizeof(a))
+using namespace std;
+const int N = 15;
+int dp[N + 1][1 << N + 10];
+int n;
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		cin>>n;
+		for(int i = 1; i <= n; i++)
+		{
+
+		}
+
+	}
+
+
+
+
+
+	return 0;
+}
