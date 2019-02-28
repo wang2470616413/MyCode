06a43cf7ea84fdcbc800b73bfbef2bd10c7af6cb
wang2470616413 <2470616413@qq.com>
2019-02-28 21:35:14 +0800

    状压dp_Hdu1074.c

--- 2019.2.26/review/dp/状压dp/状压dp_Hdu1074.c
+++ 2019.2.26/review/dp/状压dp/状压dp_Hdu1074.c
@@ -11,10 +11,11 @@ struct node
 {
 	string str;
 	int a,b;
-}
+};
 int dp[1 << N + 10];
-int const<1 << N  + 10];
+int cost[1 << N  + 10];
 node data[N + 5];
+int path[N];
 int n;
 int main()
 {
@@ -27,6 +28,28 @@ int main()
 		{
 			cin>>data[i].str>>data[i].a>>data[i].b;
 		}
+		mmset(dp,0x3f3f3f3f);
+		dp[0] = 0;
+		mmset(cost,0);
+		for(int i = 0; i < (1 << n); i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				int temp = 1 << j;
+				if((temp & i) == 0)
+				{
+					int nconst = cost[i] + data[j].b - data[j].a;
+					nconst = nconst < 0 ? 0: nconst;
+					if(dp[temp| i] > dp[i] + nconst)
+					{
+						path[j] = 
+						dp[temp | i] = dp[i] + nconst;
+						cost[temp | i] = cost[i] + data[j].b;
+					}
+				}
+			}
+		}
+		printf("%d\n",dp[(1 << n) - 1]);
 
 	}
 
