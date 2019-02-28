9832d1f1c8a10e829b1e37147ffc8f68f2c253e5
wang2470616413 <2470616413@qq.com>
2019-02-28 09:31:21 +0800

    2.c

--- 2019.2.26/review/dp/状压dp/2.c
+++ 2019.2.26/review/dp/状压dp/2.c
@@ -5,7 +5,7 @@
 #define mmset(a,b) memset(a,b,sizeof(a))
 using namespace std;
 const int N = 12;
-int dp[N + 1][1 << N + 12];
+int dp[N + 1][1 << N];
 int h,w;
 void dfs(int j, int si, int state, int nstate);
 int main()
@@ -34,15 +34,15 @@ void dfs(int j, int si, int state, int nstate)
 	{
 		int temp = 1 << j;
 		int temp1 = 1 << (j + 1);
-		if(temp & state == 0)
+		if((temp & state) == 0)
 		{
-			dfs(j+1,si,state,nstate | temp);
-			if(temp1 & state == 0 && j + 1 < w)
+			dfs(j+1,si,state,temp | nstate);
+			if(((temp1 & state) == 0) && j + 1 < w)
 			{
 				dfs(j + 2,si,state,nstate);
 			}
 		}
-		if(temp & state == 1)
+		if((temp & state) == 1)
 		{
 			dfs(j+1,si,state,nstate);
 		}
