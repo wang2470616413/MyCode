97976789389accd3c0c7992c02517027ff6f0194
wang2470616413 <2470616413@qq.com>
2019-02-28 16:00:50 +0800

    状压dp_Hdu1074.c

--- 2019.2.26/review/dp/状压dp/状压dp_Hdu1074.c
+++ 2019.2.26/review/dp/状压dp/状压dp_Hdu1074.c
@@ -7,7 +7,14 @@
 #define mmset(a,b) memset(a,b,sizeof(a))
 using namespace std;
 const int N = 15;
-int dp[N + 1][1 << N + 10];
+struct node
+{
+	string str;
+	int a,b;
+}
+int dp[1 << N + 10];
+int const<1 << N  + 10];
+node data[N + 5];
 int n;
 int main()
 {
@@ -16,9 +23,9 @@ int main()
 	while(T--)
 	{
 		cin>>n;
-		for(int i = 1; i <= n; i++)
+		for(int i = 0; i < n; i++)
 		{
-
+			cin>>data[i].str>>data[i].a>>data[i].b;
 		}
 
 	}
