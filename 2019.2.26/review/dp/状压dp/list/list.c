3d8e98e50b873a18c65494a53d1a45f46fc0d9bf
wang2470616413 <2470616413@qq.com>
2019-02-28 11:50:46 +0800

    list.c

--- 2019.2.26/review/dp/状压dp/list/list.c
+++ 2019.2.26/review/dp/状压dp/list/list.c
@@ -10,10 +10,25 @@ struct node
 	node* next;
 };
 void insertList(node* first,int index,node value);
+void printfList(node* first);
 
 int main()
 {
 
+	node* data;
+	data->next = NULL;
+	int n;
+	scanf("%d",&n);
+	for(int i = 0; i < n; i++)
+	{
+		int a,b;
+		scanf("%d %d",&a, &b);
+		node temp ;
+		temp.id = a;
+		temp.age = b;
+		insertList(data,i,temp);
+	}
+	printfList(data);
 
 
 
@@ -34,4 +49,13 @@ void insertList(node* first,int index,node value)
 	temp->age = value.age;
 	temp->next = key->next;
 	key->next = temp->next;
+}
+void printfList(node* first)
+{
+	node* temp = first->next;
+	while(temp != NULL)
+	{
+		printf("%d %d\n",temp->id,temp->age);
+		temp = temp->next;
+	}
 }
