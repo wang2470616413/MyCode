be0d133286b736ad3060819510c9be321b47cb27
wang2470616413 <2470616413@qq.com>
2019-03-01 20:19:40 +0800

    2.c

--- 2019.3.1/2.c
+++ 2019.3.1/2.c
@@ -7,40 +7,36 @@
 using namespace std;
 const int N = 1e5 + 10;
 int data[N], data1[N];
-int aux[N];
+ll aux[N];
 int num[N], mark[N];
 int n,m;
 void dfs(int p);
 int main()
 {
-	scanf("%d %d",&n,&m);
-	for(int i = 1; i <= n; i++)
+	while(~scanf("%d %d",&n,&m))
 	{
-		scanf("%d",&data[i]);
+		for(int i = 1; i <= n; i++)
+		{
+			scanf("%d",&data[i]);
+		}
+		for(int i = 1; i <= n; i++)
+		{
+			scanf("%d",&data1[i]);
+		}
+		mmset(mark,0);
+		mmset(aux,0);
+		for(int i = 1; i <= n; i++)
+		{
+			dfs(i);
+		}
+		sort(aux + 1, aux + 1 + n);
+		ll sum = 0;
+		for(int i = n; i >= n - m + 1; i--)
+		{
+			sum += aux[i];
+		}
+		printf("%lld\n",sum);
 	}
-	for(int i = 1; i <= n; i++)
-	{
-		scanf("%d",&data1[i]);
-	}
-	mmset(mark,0);
-	mmset(aux,0);
-	for(int i = 1; i <= n; i++)
-	{
-		dfs(i);
-	}
-	sort(aux + 1, aux + 1 + n);
-	int sum = 0;
-	for(int i = n; i >= n - m; i--)
-	{
-		sum += aux[i];
-	}
-	printf("%d\n",sum);
-	
-
-
-
-
-
 	return 0;
 }
 void dfs(int p)
