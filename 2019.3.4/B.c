d0f6921c3b275e3b0d5525a81edbabef7dee7250
wang2470616413 <2470616413@qq.com>
2019-03-04 01:25:23 +0800

    B.c

--- 2019.3.4/B.c
+++ 2019.3.4/B.c
@@ -6,7 +6,7 @@
 using namespace std;
 const int N = 1e3 + 5, M = 1e5 + 5;
 int data[N];
-int mark[M];
+int mark[2 * M];
 int mark1[M];
 int n;
 int main()
@@ -16,44 +16,19 @@ int main()
 	for(int i = 0; i < n; i++)
 	{
 		scanf("%d",&data[i]);
-		mark[data[i]] ++;
 	}
-	int res = 0; 
 	for(int i = 0; i < n - 1; i++)
 	{
 		for(int j = i + 1; j < n; j++)
 		{
-			int a = data[i],  b = data[j];
-			int sum = a + b;
-			int num = 0;
-			mmset(mark1,0);
-			for(int k = 0; k < n; k++)
-			{
-				mark1[data[k]] = mark[data[k]];
-			}
-			for(int k = 0; k < n; k++)if(data[k] != sum )
-			{
-				if(data[k] > sum )
-				{
-					continue;
-				}
-				if(sum % 2 == 0)
-				{
-					if(data[k] == sum / 2)
-					{
-						continue;
-					}
-				}
-				int temp1 = num;
-				num += min(mark1[data[k]],mark1[sum - data[k]]);
-				if(num != temp1)
-				{
-					mark1[data[k]] = mark1[data[k]] >= 1 ? mark1[data[k]] - 1 : 0;
-					mark1[sum - data[k]] = mark1[sum - data[k]] >= 1 ? mark1[sum - data[k]] - 1 : 0;
-				}
-			}
-			res = max(res,num);
+			int sum = data[i] + data[j];
+			mark[sum]++;
 		}
+	}
+	int res = 0;
+	for(int i = 1; i <= M; i++)
+	{
+		res = max(res,mark[i]);
 	}
 	printf("%d\n",res);
 	return 0;
