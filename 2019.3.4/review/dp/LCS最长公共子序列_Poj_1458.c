91fb2ac1495fa082bc9160051a0a556f97637921
wang2470616413 <2470616413@qq.com>
2019-03-05 16:28:59 +0800

    LCS最长公共子序列_Poj_1458.c

--- 2019.3.4/review/dp/LCS最长公共子序列_Poj_1458.c
+++ 2019.3.4/review/dp/LCS最长公共子序列_Poj_1458.c
@@ -10,21 +10,22 @@ int dp[N][N];
 int n,m;
 int main()
 {
-	mmset(dp,0);
-	scanf("%s",data1+1);
-	scanf("%s",data2+1);
-	n = strlen(data1+1);
-	m = strlen(data2+1);
-	for(int i = 1; i <= n; i++)
+	while(~scanf("%s %s",data1 + 1,data2 +1))
 	{
-		for(int j = 1; j <= m; j++)
+		mmset(dp,0);
+		n = strlen(data1+1);
+		m = strlen(data2+1);
+		for(int i = 1; i <= n; i++)
 		{
-			dp[i][j] = dp[i - 1][j - 1] + (data1[i] == data2[j] ? 1 : 0);
-			dp[i][j] = max(max(dp[i - 1][j],dp[i][j - 1]),dp[i][j]);
-		}
+			for(int j = 1; j <= m; j++)
+			{
+				dp[i][j] = dp[i - 1][j - 1] + (data1[i] == data2[j] ? 1 : 0);
+				dp[i][j] = max(max(dp[i - 1][j],dp[i][j - 1]),dp[i][j]);
+			}
 
+		}
+		printf("%d\n",dp[n][m]);
 	}
-	printf("%d\n",dp[n][m]);
 	
 	return 0;
 }
