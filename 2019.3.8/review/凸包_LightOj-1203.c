0f6c6a359238da2c9e85ffd2aac27881b5f3afea
wang2470616413 <2470616413@qq.com>
2019-03-08 16:17:58 +0800

    凸包_LightOj-1203.c

--- 2019.3.8/review/凸包_LightOj-1203.c
+++ 2019.3.8/review/凸包_LightOj-1203.c
@@ -151,11 +151,11 @@ double dis(node a, node b)
 
 double GetAng(node a,node b,node c)
 {
-	double x1 = c.x - a.x, y1 = c.y - a.y;
+	double x1 = a.x - c.x, y1 = a.y - c.y;
 	double x2 = b.x - c.x, y2 = b.y - c.y;
 	node p1,p2;
 	p1.x = x1, p1.y = y1, p2.x = x2, p2.y = y2;
 	double temp = atan2(fabs(p1.x * p2.y - p1.y * p2.x),(p1.x * p2.x + p1.y * p2.y));
 	temp = fabs(temp);
-	return temp * (180.0 / PI);
+	return temp * PI / 180.0;
 }
