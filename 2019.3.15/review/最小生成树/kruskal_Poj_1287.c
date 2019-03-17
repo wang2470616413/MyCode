754784bc417a0abec45751e06d2fd2e9ec4d2a19
wang2470616413 <2470616413@qq.com>
2019-03-17 16:35:58 +0800

    kruskal_Poj_1287.c

--- 2019.3.15/review/最小生成树/kruskal_Poj_1287.c
+++ 2019.3.15/review/最小生成树/kruskal_Poj_1287.c
@@ -5,46 +5,79 @@
 #define mmset(a,b) memset(a,b,sizeof(a))
 using namespace std;
 const int N = 55, M=  105;
-int map1[N][N];
-int mark[N];
+struct node 
+{
+	int a,b,w;
+	node ()
+	{
+
+	};
+	node (int p_x,int p_y,int p_w)
+	{
+		a = p_x;
+		b = p_y;
+		w = p_w;
+	}
+	bool operator < (const node two) const 
+	{
+		return w < two.w;
+	}
+};
+node edges[M];
+int father[N];
 int n,m;
-int dis[N];
+void init(int a,int b)
+{
+	for(int i = a; i <= b; i++)
+	{
+		father[i] = i;
+	}
+}
+int find(int x)
+{
+	return father[x] = x == father[x] ? x : find(father[x]);
+}
+void merge(int x,int y)
+{
+	int fx = find(x);
+	int fy = find(y);
+	if(fx != fy)
+	{
+		father[fx] = fy;
+	}
+}
 int main()
 {
-	while(scanf("%d", &n) && n != 0)	
+	while(scanf("%d",&n) && n != 0)
 	{
-		mmset(mark,0);
-		mmset(map1,0x3f);
+		init(1,n);
 		scanf("%d",&m);
 		for(int i = 1; i <= m; i++)
 		{
-
 			int a,b,c;
 			scanf("%d %d %d",&a,&b,&c);
-			map1[a][b] = map1[b][a] = min(c,min(map1[a][b],map1[b][a]));
+			edges[i] = node(a,b,c);
 		}
-		for(int i = 1; i <= n; i++)
-		{
-			dis[i] = map1[1][i];
-		}
-		int res = 0;
-		mark[1] = 1, dis[1] = 0; 
-		for(int i = 2; i <= n; i++)
-		{
-			int min1 = 0x3f3f3f3f,u = -1;
-			for(int j = 1; j <= n; j++) if(mark[j] == 0 && min1 > dis[j])
-			{
-				min1 = dis[j];
-				u = j;
-			}
-			res += min1;
-			mark[u] = 1;
-			for(int j = 1; j <= n; j++)if(mark[j] == 0 && dis[j] > map1[u][j])
-			{
-				dis[j] = map1[u][j];
-			}
-		}
-		printf("%d\n",res);
 	}
+	sort(edges + 1, edges + 1 + m);
+	int num = 0;
+	int res = 0;
+	for(int i = 1; i <= m; i++)
+	{
+		if(num == n)
+		{
+			break;
+		}
+		int a = find(edges[i].a);
+		int b = find(edges[i].b);
+		int w = find(edges[i].w);
+		if(find(a) != find(b))
+		{
+			res += w;
+			num++;
+		}
+		merge(a,b);
+	}
+	printf("%d\n",res);
 	return 0;
 }
