e77fcde421cf4d18dcbff9a503a009ed284eb0a4
wang2470616413 <2470616413@qq.com>
2019-03-28 20:10:24 +0800

    'Sg函数.c'

--- 2019.3.26/review/博弈/Sg函数.c
+++ 2019.3.26/review/博弈/Sg函数.c
@@ -14,9 +14,9 @@ int getSG(int n, int len)
 		mmset(mark,0);
 		for(int j = 0; j <= SG[i] && j <= len; j++)
 		{
-			mark[SG[i - f[j]]] = 1;
+			mark[SG[i - f[j]]] = 1;  //将i的次态的级数求出
 		}
-		for(int j = 0; ; j++)
+		for(int j = 0; ; j++)   //求出从0开始第一个f既不是i的次态的级数的数也不是0的数,次数即为i的级数
 		{
 			if(mark[j] == 0)
 			{
