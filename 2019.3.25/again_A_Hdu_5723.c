dcdad6e54a2e15f93e73248120862f1db4a5d187
wang2470616413 <2470616413@qq.com>
2019-03-28 21:48:54 +0800

    'again_A_Hdu_5723.c'

---
+++ 2019.3.25/again_A_Hdu_5723.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<algorithm>
+#include<vector>
+#define ll long long 
+#define mmset(a,b) memset(a,b,sizeof(a))
+using namespace std;
+const int N = 100005, M = 1000005;
+struct node
+{
+	int a,b,w;
+	node()
+	{
+
+	};
+	node(int )
+}
+int father[N];
+
+int main()
+{
+
+
+
+
+	return 0;
+}
+
