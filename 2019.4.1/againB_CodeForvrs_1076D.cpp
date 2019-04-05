ad066c4ff6159634bffbb412373d4e98816119e7
wang2470616413 <2470616413@qq.com>
2019-04-05 21:19:36 +0800

    'againB_CodeForvrs_1076D.cpp'

--- 2019.4.1/againB_CodeForvrs_1076D.cpp
+++ 2019.4.1/againB_CodeForvrs_1076D.cpp
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<string.h>
+#include<vector>
+#include<queue>
 #include<algorithm>
 #define ll long long 
 #define mmset(a,b) memset(a,b,sizeof(a))
@@ -7,34 +9,93 @@ using namespace std;
 const int N = 3e5 + 5;
 struct node
 {
-	int to,w,next;
+	int to,next;
+	ll w;
 	node()
 	{
 		
 	};
-	node(int p_to, int p_w, int p_next)
+	node(int p_to, ll p_w, int p_next)
 	{
 		to = p_to;
 		w = p_w;
 		next = p_next;
 	};
 };
+struct edge
+{
+	int p;
+	ll w;
+	edge()
+	{
+		
+	};
+	edge(int p_p, ll p_w)
+	{
+		p = p_p;
+		w = p_w;
+	};
+	bool operator < (const edge two) const
+	{
+		return w > two.w;
+	}
+};
 int head[N];
-node edges[N];
-void addEdge(int a,int cb, int c,int& len)
+ll dis[N];
+vector <int> ans;
+node edges[2 * N];
+int pre[N];
+int n,m,k;
+void addEdge(int a,int b, int c,int& len)
 {
 	edges[len] = node(b,c,head[a]);
 	head[a] = len++;
 }
-void dijkstra(int s, int e)
+void dijkstra(int s, int e,int k)
 {
-	
+	mmset(dis,0x3f);
+	dis[s] = 0;
+	priority_queue <edge> que;
+	que.push(edge(s,0));
+	while(!que.empty() && ans.size() <= k)
+	{
+		edge temp = que.top();
+		que.pop();
+		if(temp.w > dis[temp.p])
+		{
+			continue;
+		}
+		int u = temp.p;
+		ans.push_back(pre[u]);
+		for(int j = head[u]; j != -1; j = edges[j].next)if(dis[edges[j].to] > dis[u] + edges[j].w) 
+		{
+			pre[edges[j].to] = (j + 1) / 2;
+			dis[edges[j].to] = dis[u] + edges[j].w;
+			que.push(edge(edges[j].to,edges[j].w + dis[u]));
+		}
+	}
 }
 int main()
 {
 	
-	
-	
+		scanf("%d %d %d",&n,&m,&k);
+		ans.clear();
+		mmset(head,-1);
+		int a,b,c;
+		int len = 1;
+		for(int i = 1; i <= m; i++)
+		{
+			scanf("%d %d %d",&a,&b,&c);
+			addEdge(a,b,c,len);
+			addEdge(b,a,c,len);
+		}
+		dijkstra(1,n,k);
+		printf("%d\n",ans.size() - 1);
+		for(int i = 1; i < ans.size(); i++)
+		{
+			printf("%d ",ans[i]);
+		}
+		printf("\n");
 	
 	return 0;
 }
