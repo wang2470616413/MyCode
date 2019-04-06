d10b5bf1f001ef29e2cb75bbf0ac498c1cf990a8
wang2470616413 <2470616413@qq.com>
2019-04-06 17:00:36 +0800

    'againC_Zoj_3017.cpp'

---
+++ 2019.3.20/againC_Zoj_3017.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<string.h>
+#include<algorithm>
+#define ll long long 
+#define mmset(a,b) memset(a,b,sizeof(a))
+using namespace std;
+const int N = 105;
+int map1[N][N],data[N][N];
+int dp[N][N][N];
+int n,m,z;
+
+int main()
+{
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+		mmset(map1,0);
+		scanf("%d %d %d",&n,&m,&z);
+		for(int i = 1; i <= m; i++)
+		{
+			for(int j = 2; j <= n; j++)
+			{
+				scanf("%d",&map1[i][j]);
+			}
+		}
+		for(int i = 1; i <= m; i++) 
+		{
+			for(int j = 1; j <= m; j++) 
+			{
+				scanf("%d",&data[i][j]);
+				if(i == j)
+				{
+					data[i][j] = 0;
+				}
+			}
+		}
+		mmset(dp,0x3f);
+		dp[z][1][0] = 0;
+		for(int k = 1; k <= z; k++)
+		{
+			for(int j = 1; j <= n; j++)
+			{
+				for(int i = 1; i <= m; i++)
+				{
+					for(int q = 1; q <= m; q++)if(k >= data[q][i])
+					{
+						 dp[k][i][j] = min(dp[k][i][j],min(dp[k - data[q][i]][q][j],dp[k - data[q][i]][q][j-1] + map1[q][j]));
+						 printf("%d\n",dp[k][i][j]);
+					}
+				}
+			}
+		}
+		for(int j = 1; j <= z; j++)
+		{
+			printf("%d ",dp[j][m][n]);
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+	return 0;
+}
