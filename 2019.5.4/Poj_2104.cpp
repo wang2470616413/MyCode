a3417f7346ddc9b5084fbf078ac6a4038c11b341
wang2470616413 <2470616413@qq.com>
2019-05-11 10:59:22 +0800

    主席树模板题

--- 2019.5.4/Poj_2104.cpp
+++ 2019.5.4/Poj_2104.cpp
@@ -1,112 +1,100 @@
 #include<stdio.h>
 #include<string.h>
 #include<algorithm>
-#define ll long long
+#define ll long long 
 #define mmset(a,b) memset(a,b,sizeof(a))
 #define lson l,m,lts[rt]
-#define rson m+1,r, rts[rt]
+#define rson m + 1, r, rts[rt] 
 using namespace std;
-const int N = 1005;
-int lts[N << 2], rts[N << 2], root[N << 2];
-int data[N << 2], Hash[N << 2];
-int tot = 0,n,m;
-void Build(int l ,int r, int &rt)
+const int N = 1e5 + 5;
+int rts[N * 30],lts[N * 30],roots[N];
+int tree[N * 30],data[N],HashData[N];
+int n,m;
+int tot = 0;
+void Build(int l, int r, int& rt)
 {
     rt = ++tot;
-    root[rt] = 0;
-    if(l == r)
-    {
-    	return ;
-    }
-    int m = (l + r) >> 1;
-    Build(lson);
-    Build(lson);
-}
-void Update(int p, int num, int l, int r, int& rt, int lst)
-{
-    rt = ++tot;
-    lts[rt] = lts[lst];
-    rts[rt] = rts[lst];
-    root[rt] = root[lst] + num;
+    tree[rt] = 0;
     if(l == r)
     {
         return ;
     }
+    int m = (l + r) >> 1; 
+    Build(lson);
+    Build(rson);
+}
+void Update(int p, int num, int l, int r, int& rt, int last)
+{
+    rt = ++tot;
+    lts[rt] = lts[last], rts[rt] = rts[last];
+    tree[rt] = tree[last] + num;
+    if(l == r)
+	{
+		return ;
+    }
+    int m = (l + r) >> 1;
+    if(p <= m)
+    {
+        Update(p,num,lson,lts[rt]);
+    }
     else
     {
-        int m = (l + r) >> 1;
-        if(p <= m)
-        {
-            Update(p,num,lson,lts[rt]);
-        }
-        else
-        {
-            Update(p,num,rson,rts[rt]);
-        }
-        
+        Update(p,num,rson,rts[rt]);
     }
-    
 }
-int query(int a,int b, int l, int r, int k)
+int query(int a, int b, int l, int r, int k)
 {
     if(l == r)
     {
-            return l ;
+        return l;
     }
     else
     {
-        int c = root[lts[b]] - root[lts[a]];
+        int numc = tree[lts[b]] - tree[lts[a]];
         int m = (l + r) >> 1;
-        if(c >=k)
+        if(k <= numc)
         {
-            return query(lts[a],lts[b],1,m,k);
+            return query(lts[a],lts[b],l,m,k);
         }
         else
         {
-            return query(rts[a],rts[b],1,m,k);
+            return query(rts[a],rts[b],m+1,r,k-numc);
         }
-        
     }
-    
 }
+
+
+/*
+7 3
+1 5 2 6 3 7 4
+2 5 3
+4 4 1
+1 7 3
+*/
 int main()
 {
-    while(~scanf("%d%d",&n,&m))
+    scanf("%d %d",&n,&m);
+    for(int i = 1; i <= n; i++)
     {
-        tot = 0;
-        for(int i = 1;  i <= n; i++)
-        {
-            scanf("%d",&data[i]);
-            Hash[i] = data[i];
-        }
-        sort(Hash + 1, Hash + n + 1);
-        int len = unique(Hash + 1, Hash + n + 1) - (Hash + 1);
-        // for(int i = 1; i <= len; i++)
-        // {
-        //     printf("%d ",Hash[i]);
-        // }
-        // printf("\n");
-        Build(1,len,root[0]);
-        for(int i = 1; i <= n; i++)
-        {
-            data[i] = lower_bound(Hash + 1, Hash + n + 1,data[i]) - Hash;
-        }
-        for(int i = 1; i <= n; i++)
-        {
-            printf("%d ",data[i]);
-        }
-        printf("\n");
-        for(int i = 1; i <= n; i++)
-        {
-            Update(data[i],1,1,len,root[i],root[i-1]);
-        }
-        int a,b,c;
-        for(int i = 1; i <= m; i++)
-        {
-            scanf("%d %d %d",&a,&b,&c);
-            printf("%d\n",Hash[query(root[a - 1],root[b],1,len,c)]);
-    	}
+        scanf("%d",&data[i]);
+        HashData[i] = data[i];
     }
+    sort(HashData + 1, HashData + 1 + n);
+    int len = unique(HashData + 1, HashData + 1 +n) - (HashData + 1);
+    Build(1,len,roots[0]);
+    for(int i =  1; i <= n; i++)
+    {
+        int num = lower_bound(HashData + 1, HashData + 1 + n,data[i]) - HashData;
+        Update(num,1,1,len,roots[i], roots[i-1]);
+    }
+    for(int i = 1; i <= m; i++)
+    {
+        int a,b,c;
+        scanf("%d %d %d",&a,&b,&c);
+        printf("%d\n",HashData[query(roots[a-1],roots[b],1,len,c)]);
+    }
+    
+    
 
 
 
