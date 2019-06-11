a02df6b78e4f27608f60a766c9585ef426a1a563
wang2470616413 <2470616413@qq.com>
2019-06-11 21:05:33 +0800

    主席树模板，区间第k小

--- 2019.6.5/主席树_模板.cpp
+++ 2019.6.5/主席树_模板.cpp
@@ -3,59 +3,110 @@
 #include<algorithm>
 #define ll long long 
 #define mmset(a,b) memset(a,b,sizeof(a))
-#define lson l,m,rt<<1
-#define rson m + 1, r, rt << 1| 1 
 #define hash hash1
 using namespace std;
-const int N =  1e5 + 5;
-int data[N],hash[N];
-int ls[N * 30], rs[N * 30], tree[N * 30];
-int tot = 0;
-void add(int p, int C,int l, int r,int& rt,int last)
+const int N = 1e5 + 5;
+int ls[N * 30],rs[N * 30], ts[N * 30], root[N];
+int tree[N * 30];
+int data[N], hash[N];
+int n,m,tot;
+void build(int l,int r, int& rt)
 {
 	rt = tot++;
-	ls[rt] = ls[last],rs[rt] = rs[last];
-	tree[rt] = tree[last]++;
-    if(l == r)
-    {
-        return;
-    }
-    int m = (l + r) >> 1;
-    if(p <= m)
-    {
-        add(p,C,l,m,ls[rt],ls[rt]);
-    }
-    else
-    {
-        add(p,C,m+1,r,rs[rt],rs[rt]);
-    }
+	tree[rt] = 0;
+	if(l == r)
+	{
+		return;
+	}
+	else
+	{
+		int m = (l + r) >> 1;
+		build(l,m,ls[rt]);
+		build(m+1,r,rs[rt]);
+	}
+	
 }
+void add(int p, int C, int l, int r, int& rt, int last)
+{
+	rt = tot++;
+	ls[rt] = ls[last];
+	rs[rt] = rs[last];
+	tree[rt] = tree[last] + C;
+	if(l == r)
+	{
+		return;
+	}
+	else
+	{
+		int m = (l + r) >> 1;
+		if(p <= m)
+		{
+			add(p,C,l,m,ls[rt],ls[rt]);
+		}
+		if(p >= m + 1)
+		{
+			add(p,C,m + 1, r, rs[rt], rs[rt]);
+		}
+	}
+	
+}
+int query(int x, int y, int l, int r, int k)
+{
+	if(l == r)
+	{
+		return l;
+	}
+	else
+	{
+		int m = (l + r) >> 1;
+		int cnt = tree[ls[y]] - tree[ls[x]];
+		if(k <= cnt)
+		{
+			return query(ls[x],ls[y],l,m,k);
+		}
+		else
+		{
+			return query(rs[x],rs[y],m+1,r,k - cnt);
+		}
+	}
+	
+
+}
+
 /*
-12 6
-3 8 13 15 5 8 12 9 3 6 21 22
-1 4 6
-4 7 9
-6 7 7
-3 1 2
-9 1 7
-4 1 9
-##
-*/
+7 3
+1 5 2 6 3 7 4
+2 5 3
+4 4 1
+1 7 3
+Sample Output
+5
+6
+3
+ */
 int main()
 {
-	int n,m;
+	tot = 1;
 	scanf("%d %d",&n,&m);
 	for(int i = 1; i <= n; i++)
 	{
 		scanf("%d",&data[i]);
-		hash[i]  = data[i];
+		hash[i] = data[i];
 	}
 	sort(hash + 1, hash + 1 + n);
-//	int len = unique(hash + 1, hash + 1 + n) - (hash + 1);
+	int len = unique(hash + 1, hash + 1 + n) -  (hash + 1);
+	build(1,len,root[0]);
 	for(int i = 1; i <= n; i++)
 	{
-		int res = lower_bound(hash + 1, hash + 1 + n,data[i]) - hash;
-		printf("%d %d\n",data[i],res);
+		int a = lower_bound(hash + 1, hash + 1 + n, data[i]) - hash;
+		add(a,1,1,len,root[i],root[i-1]);
+	}
+	for(int i = 1; i <= m; i++)
+	{
+		int a,b,k;
+		scanf("%d %d %d",&a,&b,&k);
+		int res = hash[query(root[a-1],root[b],1,len,k)];
+		printf("%d\n",res);
 	}
 	return 0;
 }
