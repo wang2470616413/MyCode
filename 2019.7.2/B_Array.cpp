a3dadbe4a630e5723d06518bce0fea55198eb7af
wang2470616413 <2470616413@qq.com>
2019-07-03 21:14:16 +0800

    Array

--- 2019.7.2/B_Array.cpp
+++ 2019.7.2/B_Array.cpp
@@ -1,81 +1,54 @@
 #include<stdio.h>
 #include<string.h>
 #include<algorithm>
-#define ll long long 
+#define ll long long
 #define mmset(a,b) memset(a,b,sizeof(a))
+#define index index0
 using namespace std;
 const int N = 5e3 + 5;
-const ll MOD = 1e9 + 7;
-int dp[2][N];
-int ans[N];
-int n,k,cnt, cur;
-struct Query { int n, k, index; } query[N];
-
+const int MOD = 1e9 + 7;
+struct node
+{
+    int a,b,m;
+    bool operator < (node two) const
+    {
+        return  a < two.a;
+    }
+};
+int dp[2][N] , ans[N], n,m, index = 1;
+node Query[N];
 int main()
 {
-    //  int data[100];
-    // int aux[100];
-    // for(int i = 1; i <= 10; i++)
-    // {
-    //     mmset(data,0);
-    //     for(int k = 1; k <= i; k++)
-    //     {
-    //         aux[k] = k;
-    //     }
-    //     do 
-    //     {
-    //         int num = 0;
-    //         for(int j = 1; j <= i; j++)
-    //         {
-    //             for(int q = j + 1; q <= i; q++)
-    //             {
-    //                 if(aux[j] > aux[q])
-    //                 {
-    //                     num++;
-    //                 }
-    //             }
-    //         }
-    //         data[num]++;
-    //     }while (next_permutation(aux + 1, aux + 1 + i));
-    //     for(int j = 1; j <= 20; j++)
-    //     {
-    //         printf("%d ",data[j]);
-    //     }
-    //     printf("\n");
-    // }
-    
-
-   while(~scanf("%d %d", &query[cnt].n, &query[cnt].k))
-   {
-        query[cnt].index = cnt;
-        cnt++;
-   }
-
-    mmset(dp,0);
+    int num = 1;
+    while(~scanf("%d %d",&Query[num].a, &Query[num].b))
+    {
+        Query[num].m = num;
+        num++;
+    }
+    sort(Query + 1, Query + num);
     dp[0][0] = 1;
     for(int i = 1; i <= 5000; i++)
     {
         int sum = 0;
         for(int j = 0; j <= 5000; j++)
         {
-            if(j < i)
+          
+            sum = ((sum % MOD) + dp[(i + 1) & 1][j]) % MOD;
+            if(j >= i)
             {
-                sum = (sum % MOD)  + (dp[(i + 1) & 1][j]) % MOD;
+                sum = ((sum % MOD) - dp[(i + 1) & 1][j - i] + MOD) % MOD;
             }
-            else
-            {
-                sum = (sum % MOD) - (dp[(i + 1) & 1][j - i]) % MOD;
-            }
-            
-            dp[i & 1][j] = (sum % MOD) % MOD; 
+            dp[i & 1][j] = sum;
         }
-             while (cur < cnt && query[cur].n == i) 
-             {
-            ans[query[cur].index] = dp[i & 1][query[cur].k];
-            cur++;
+        while(index < num && Query[index].a == i)
+        {
+            ans[Query[index].m] = dp[i & 1][Query[index].b];
+            index++;
         }
     }
-    for (int i = 0; i < cnt; i++) printf("%d\n", ans[i]);
-
+    for(int i = 1; i < num; i++)
+    {
+        printf("%d\n",ans[i]);
+    }
     return 0;
 }
